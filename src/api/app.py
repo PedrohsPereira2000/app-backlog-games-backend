c030b6b78d9b7e036d666c5c269c4f704f0755ad
Pedro Pereira <pedrohspereira2000@gmail.com>
2024-02-28 14:52:33 -0300

    Alterando do main.py para o api/app.py

--- src/api/app.py
+++ src/api/app.py
@@ -1,3 +1,4 @@
+import uvicorn
 from flask import Flask, jsonify, request
 from flask_cors import CORS
 from models.User import User
@@ -6,17 +7,15 @@ from controller.User import create_user, verify_user, get_user_id_by_email, sear
 app = Flask(__name__)
 CORS(app)
 
-@app.route("/test_router", methods=['GET'])
-def test_root():
+@app.route("/", methods=['GET'])
+def test():
     return jsonify({'message': 'A rota está funcionando!'})
 
 @app.route("/login", methods=['POST'])
-def auth_user():
+def login():
     data = request.json
-    print(data)
     if 'user_email' in data and 'user_password' in data:
         if verify_user(data['user_email'], data['user_password']):
-            print(get_user_id_by_email(data['user_email']))
             return jsonify(
                 {
                     "user_id": get_user_id_by_email(data['user_email'])
@@ -27,6 +26,15 @@ def auth_user():
 
     else:
         return jsonify({"error": "Está fantando informação"})
+    
+@app.route("/user/<user_id>", methods=['GET'])
+def get_backlog_by_user(user_id):
+    user = search_user_by_id(user_id)
+    if user is None:
+        return jsonify({"error": "O Id do usuário não foi informado, ou está errado"})
+    return jsonify(
+        {"Success": user}
+    )
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=8000, debug=True)
+    app.run()
